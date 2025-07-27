d93af412857810cb8eb2521894923390704fbd2e
Jerod Herzog <jdhrzg@gmail.com>
2025-07-27 16:27:46 -0500

    SetInterval works

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ async function onGetQuoteClick() {
         console.log(quote);
         console.log(data);
 
-        document.getElementById("quoteDisplay").innerHTML += quote.toHTML();
+        document.getElementById("quoteDisplay").innerHTML = quote.toHTML();
     }
 }
 
 async function makeRequest(url) {
     try {
-        // Bypass CORS
+        // Bypass CORS - This still confuses me
         const proxyUrl = "https://api.allorigins.win/raw?url=";
 
         const response = await fetch(proxyUrl + url);
@@ -45,4 +45,38 @@ async function makeRequest(url) {
         console.error(error.message);
         return null
     }
+}
+
+
+let count = 0;
+let timeoutId = null;
+
+const startIntervalBtn = document.getElementById("startIntervalBtn");
+const stopIntervalBtn = document.getElementById("stopIntervalBtn");
+const timeoutOutput = document.getElementById("timeoutOutput");
+
+startIntervalBtn.addEventListener("click", () => {
+    if (timeoutId !== null) return;
+
+    timeoutId = setInterval(() => {
+        const outputString = `Interval ran ${++count} times | ${getCurrentTime()}`;
+        timeoutOutput.textContent = outputString;
+        console.log(outputString);
+    }, 1000);
+});
+
+stopIntervalBtn.addEventListener("click", () => {
+    clearInterval(timeoutId);
+    timeoutId = null;
+    timeoutOutput.textContent = "Interval stopped";
+});
+
+// Seriously, no "hh:mm:ss.ffff" formatting? :)
+function getCurrentTime() {
+  const now = new Date();
+  const h = now.getHours().toString().padStart(2, '0');
+  const m = now.getMinutes().toString().padStart(2, '0');
+  const s = now.getSeconds().toString().padStart(2, '0');
+  const ms = now.getMilliseconds().toString().padStart(3, '0');
+  return `${h}:${m}:${s}.${ms}`;
 }
